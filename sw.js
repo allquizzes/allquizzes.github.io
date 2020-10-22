a2875daddf54e40e2c06b28ef9de77bd7291a8b9
allquizzes <72504380+allquizzes@users.noreply.github.com>
2020-10-22 09:50:43 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('all-quizzes').then(function(cache) {
+     return cache.addAll([
+       '/index.html'
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
