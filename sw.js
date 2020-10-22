7c751ba718c9b75d21d81dd9d45ca5202405ddd6
allquizzes <72504380+allquizzes@users.noreply.github.com>
2020-10-22 12:58:38 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,34 +1,12 @@
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('v1').then(function(cache) {
-      return cache.addAll([
-        '/index.html' ,
-        '/style.css'
-      ]);
-    })
-  );
-});
+self.addEventListener('install', () => {
+    console.log(`installing service worker`);
+})
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(caches.match(event.request).then(function(response) {
-    // caches.match() always resolves
-    // but in case of success response will have value
-    if (response !== undefined) {
-      return response;
-    } else {
-      return fetch(event.request).then(function (response) {
-        // response may be used only once
-        // we need to save clone to put one copy in cache
-        // and serve second one
-        let responseClone = response.clone();
-        
-        caches.open('v1').then(function (cache) {
-          cache.put(event.request, responseClone);
-        });
-        return response;
-      }).catch(function () {
-        return caches.match('/index.html');
-      });
-    }
-  }));
-});
+self.addEventListener('activate', () => {
+    console.log(`activating service worker`);
+})
+
+self.addEventListener('fetch', event => {
+    console.log(`fetching...
+    ${event.request.url}`);
+})
