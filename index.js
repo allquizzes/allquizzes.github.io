faa7f65caf045d1644ce2d785e8f05148903bdba
allquizzes <72504380+allquizzes@users.noreply.github.com>
2020-10-27 17:56:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,3 +34,13 @@ window.addEventListener('beforeinstallprompt', (e) => {
       });
   });
 });
+function display() {
+Notification.requestPermission(function(status) {
+    console.log('Notification permission status:', status);
+});
+  if (Notification.permission == 'granted') {
+    navigator.serviceWorker.getRegistration().then(function(reg) {
+      reg.showNotification('Hello world!');
+    });
+  }
+}
