2846bd424872831dc20503824c272ae7f8aec158
allquizzes <72504380+allquizzes@users.noreply.github.com>
2020-12-24 23:47:02 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,6 @@ self.addEventListener('install', function(e) {
    caches.open('fox-store').then(function(cache) {
      return cache.addAll([
        'IMG-20201127-WA0043.jpg' ,
-       'index.html' , 
        'https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js' ,
        'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js' ,
        'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js' ,
