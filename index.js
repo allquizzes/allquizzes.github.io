6f8f8128419f6afd80db3577026f0525a55101f9
Mohammad Omar Farooq Khan <72504380+allquizzes@users.noreply.github.com>
2021-05-17 22:06:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -37,3 +37,19 @@ window.addEventListener('beforeinstallprompt', (e) => {
       });
   });
 });
+document.querySelector('.cache-article').addEventListener('click', function (event) {
+  event.preventDefault();
+
+  var id = this.dataset.articleId;
+  caches.open('mysite-article-' + id).then(function (cache) {
+    fetch('/get-article-urls?id=' + id)
+      .then(function (response) {
+        // /get-article-urls returns a JSON-encoded array of
+        // resource URLs that a given article depends on
+        return response.json();
+      })
+      .then(function (urls) {
+        cache.addAll(urls);
+      });
+  });
+});
