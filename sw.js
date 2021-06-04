9579d874c0e5c658fb426d24a8e78a6b1ea2c012
Mohammad Omar Farooq Khan <72504380+allquizzes@users.noreply.github.com>
2021-06-05 00:54:27 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,12 @@
-self.addEventListener('fetch', function (event) {
-  event.respondWith(
-    caches.open('mysite-dynamic').then(function (cache) {
-      return fetch(event.request).then(function (response) {
-        cache.put(event.request, response.clone());
-        return response;
-      });
-    }),
-  );
-});
+const OFFLINE_VERSION = 3;
+const CACHE_NAME = 'offline';
+// Customize this with a different URL if needed.
+const OFFLINE_URL = 'offline.html';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil((async () => {
+    const cache = await caches.open(CACHE_NAME);
+    // Setting {cache: 'reload'} in the new request will ensure that the response
+    // isn't fulfilled from the HTTP cache; i.e., it will be from the network.
+    await cache.add(new Request(OFFLINE_URL, {cache: 'reload'}));
+  })());
