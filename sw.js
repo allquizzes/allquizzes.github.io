d8a0d51870ee4ac20a1f8c628f02618d55eaf001
Mohammad Omar Farooq Khan <72504380+allquizzes@users.noreply.github.com>
2021-06-08 17:28:13 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ self.addEventListener('install', function(e) {
  e.waitUntil(
    caches.open('fox-store').then(function(cache) {
      return cache.addAll([
-'index.html' , 'index.js'
+'index.html' , 'index.js' , 'sw.js'
      ]);
    })
  );
