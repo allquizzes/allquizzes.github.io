43d5da55cae18d475533213e704678eac9e7192e
Mohammad Omar Farooq Khan <72504380+allquizzes@users.noreply.github.com>
2021-06-08 12:44:02 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,10 +9,3 @@ self.addEventListener('install', function (event) {
     }),
   );
 });
-self.addEventListener('fetch', function (event) {
-  event.respondWith(
-    fetch(event.request).catch(function () {
-      return caches.match(event.request);
-    }),
-  );
-});
