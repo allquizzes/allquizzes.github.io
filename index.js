5e9efbf112ad8f6d2ce3f5c247f0e6954f6b2c51
Mohammad Omar Farooq Khan <72504380+allquizzes@users.noreply.github.com>
2021-06-08 12:40:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
     navigator.serviceWorker.register('/sw.js').then(function(registration) {
       // Registration was successful
-      alert('ServiceWorker registration successful with scope: ', registration.scope);
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, function(err) {
       // registration failed :(
-      alert('ServiceWorker registration failed: ', err);
+      console.log('ServiceWorker registration failed: ', err);
     });
   });
 }
