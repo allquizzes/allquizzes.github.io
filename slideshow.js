16b0817c7120485cf2c245d2b616e2ac94c283af
Mohammad Omar Farooq Khan <72504380+allquizzes@users.noreply.github.com>
2021-06-16 19:13:07 +0530

    Update slideshow.js

--- slideshow.js
+++ slideshow.js
@@ -37,25 +37,3 @@ function showSlides(n) {
   }
 slides[slideIndex-1].style.display = "block";
 }
-var logo = document.createElement("BUTTON");            
-logo.onclick = "clickkey()" ;
-logo.innerHTML = "Click" ;         
-document.getElementsByTagName("body")[0].appendChild(logo);  
-function clickkey() {
-var keyboardEvent = document.createEvent('KeyboardEvent');
-var initMethod = typeof keyboardEvent.initKeyboardEvent !== 'undefined' ? 'initKeyboardEvent' : 'initKeyEvent';
-
-keyboardEvent[initMethod](
-  'keydown', // event type: keydown, keyup, keypress
-  true, // bubbles
-  true, // cancelable
-  window, // view: should be window
-  false, // ctrlKey
-  false, // altKey
-  false, // shiftKey
-  false, // metaKey
-  39, // keyCode: unsigned long - the virtual key code, else 0
-  0, // charCode: unsigned long - the Unicode character associated with the depressed key, else 0
-);
-document.dispatchEvent(keyboardEvent);
-}
