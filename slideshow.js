86528e2f503cb8d8a1ce5c34375fadcd719d7035
Mohammad Omar Farooq Khan <72504380+allquizzes@users.noreply.github.com>
2021-06-16 12:19:57 +0530

    Update slideshow.js

--- slideshow.js
+++ slideshow.js
@@ -25,6 +25,23 @@ function checkKey(e) {
     }
 
 }
+var keyboardEvent = document.createEvent('KeyboardEvent');
+var initMethod = typeof keyboardEvent.initKeyboardEvent !== 'undefined' ? 'initKeyboardEvent' : 'initKeyEvent';
+
+keyboardEvent[initMethod](
+  'keydown', // event type: keydown, keyup, keypress
+  true, // bubbles
+  true, // cancelable
+  window, // view: should be window
+  false, // ctrlKey
+  false, // altKey
+  false, // shiftKey
+  false, // metaKey
+  37, // keyCode: unsigned long - the virtual key code, else 0
+  0, // charCode: unsigned long - the Unicode character associated with the depressed key, else 0
+);
+document.dispatchEvent(keyboardEvent);
+
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("con");
