2f5cf7bacaa525402bad243b56514e122f97adc9
Mohammad Omar Farooq Khan <72504380+allquizzes@users.noreply.github.com>
2021-06-22 19:18:27 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,33 @@
-self.addEventListener('install', function(e) {
- e.waitUntil(
-   caches.open('fox-store').then(function(cache) {
-     return cache.addAll([
-'index.html' , 'index.js' , 'sw.js'
-     ]);
-   })
- );
-});
-
-self.addEventListener('fetch', function(e) {
-  console.log(e.request.url);
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+'index.html' , 'style.css' 
+      ]);
     })
   );
 });
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(caches.match(event.request).then(function(response) {
+    // caches.match() always resolves
+    // but in case of success response will have value
+    if (response !== undefined) {
+      return response;
+    } else {
+      return fetch(event.request).then(function (response) {
+        // response may be used only once
+        // we need to save clone to put one copy in cache
+        // and serve second one
+        let responseClone = response.clone();
+        
+        caches.open('v1').then(function (cache) {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      }).catch(function () {
+        return caches.match('/index.html');
+      });
+    }
+  }));
+})
